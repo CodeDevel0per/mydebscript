0dc4c8eaf1ff41e95aa68bbce7de4a8aae54b6f4
C0deDeveloper <83279568+CodeDevel0per@users.noreply.github.com>
2023-07-22 19:56:44 +0530

    Main Script

---
+++ script.sh
@@ -0,0 +1,22 @@
+echo "Please wait here!"
+sudo apt install nala -y
+sudo apt install wget -y
+sudo nala fetch
+mkdir DebInstall
+cd DebInstall
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo nala install ./google-chrome-stable_current_amd64.deb
+
+sudo nala install micro neofetch -y
+cd ~
+echo "noefetch" >> .bashrc
+
+sudo nala install curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo nala update
+
+sudo nala install brave-browser
